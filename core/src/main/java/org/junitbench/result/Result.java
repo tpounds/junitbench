2554e072cbe067abbe7f3bc70dae9f53e597d39a
Trevor Pounds <trevor.pounds@gmail.com>
2009-08-21 23:08:27 +0000

    import initial result generation engines

---
+++ core/src/main/java/org/junitbench/result/Result.java
@@ -0,0 +1,10 @@
+package org.junitbench.result;
+
+public class Result
+{
+   public String  ID;
+   public int     threadNumber;
+   public boolean error;
+//   public String  errorMessage;
+   public long    time;
+}
